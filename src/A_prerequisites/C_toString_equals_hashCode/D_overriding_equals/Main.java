db53931808da55efd4b50f1f211b2f3c5272c9a8
moshe <moshe.lach@yahoo.com>
2024-09-04 12:20:34 -0400

    correction

--- src/A_prerequisites/C_toString_equals_hashCode/D_overriding_equals/Main.java
+++ src/A_prerequisites/C_toString_equals_hashCode/D_overriding_equals/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         // Both of the following call the version of the equals method that is written in the Student class.
         System.out.println(s1.equals(s2)); // true
-        System.out.println(s1.equals(s3)); // false
+        System.out.println(s1.equals(s3)); // true
 
         System.out.println(s1.hashCode());
         System.out.println(s2.hashCode());
